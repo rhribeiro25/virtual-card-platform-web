c0c9aced6d7c2e18d45467915c5d1d5b92611d45
rhribeiro25 <renan.shaolin.25@gmail.com>
2025-07-12 18:20:31 +0100

    adding footer and home components with styles

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# VirtualCardPlatformWeb
+# 🎫 Virtual Card Platform
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 20.1.0.
 
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from "./presentation/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('virtual-card-platform-web-2');
+  protected readonly title = signal('virtual-card-platform-web');
 }
