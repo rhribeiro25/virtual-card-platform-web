a65dceaa6a50ca724fcbdd22c334ac3e121b3e7a
rhribeiro25 <renan.shaolin.25@gmail.com>
2025-07-13 17:28:40 +0100

    creating add card form

--- src/app/presentation/cards/card-form/card-form.ts
+++ src/app/presentation/cards/card-form/card-form.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Button } from '../../button/button';
+
 
 @Component({
   selector: 'app-card-form',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, Button],
   templateUrl: './card-form.html',
-  styleUrl: './card-form.css'
+  styleUrls: ['./card-form.css'],
 })
 export class CardForm {
+  
+  private fb = inject(FormBuilder);
 
+  submitted = signal(false);
+
+  form = this.fb.group({
+    name: ['', [
+      Validators.required, 
+      Validators.minLength(3)]],
+    balance: [0, [
+      Validators.required, 
+      Validators.min(0)]],
+  });
+
+  onSubmit() {
+    this.submitted.set(true);
+    if (this.form.valid) {
+      console.log('✅ Card submitted:', this.form.value);
+      // call your service here
+    }
+  }
 }
