010a9de156ecca2c782ef4998a501f64b1ae807c
rhribeiro25 <renan.shaolin.25@gmail.com>
2025-07-20 23:02:51 +0100

    cart details done

--- src/app/presentation/cards/card-form/card-form.ts
+++ src/app/presentation/cards/card-form/card-form.ts
@@ -1,20 +1,29 @@
 import { Component, signal, inject } from '@angular/core';
-import { ReactiveFormsModule, FormBuilder, Validators, NonNullableFormBuilder } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Button } from '../../button/button';
 import { CreateCardUseCase } from '../../../application/use-cases/card/create-card.usecase';
 import { Router } from '@angular/router';
 import { MatSnackBarModule, MatSnackBar } from '@angular/material/snack-bar';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-card-form',
   standalone: true,
   imports: [
-    CommonModule, 
-    ReactiveFormsModule, 
-    Button, 
-    MatSnackBarModule
+    CommonModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    MatDividerModule,
+    Button
   ],
   templateUrl: './card-form.html',
   styleUrls: ['./card-form.css'],
@@ -42,6 +51,7 @@ export class CardForm {
           this.snackBar.open('✅ Card created successfully!', 'Close', {
             duration: 3000,
             verticalPosition: 'top',
+            horizontalPosition: 'right',
           });
           this.router.navigate(['cards/details'], {
             state: { card: response }
